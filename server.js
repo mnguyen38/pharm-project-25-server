b245f00998dda15e415eaaf384449dddcf02a66d
Phillip Nguyen <nh273@cornell.edu>
2025-03-08 13:02:11 -0500

    just using slightly diff client configs

--- server.js
+++ server.js
@@ -19,15 +19,52 @@ app.use(express.json());
 
 dotenv.config();
 const mongoUri = process.env.MONGO_URI;
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
 mongoose
-  .connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(mongoUri, clientOptions)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 DrugCatalogRoutes(app);
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "uploaded_files"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Endpoint to handle PDF file upload
+app.post("/uploadPdf", upload.single("pdf"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  try {
+    const pdfFilePath = path.join(
+      __dirname,
+      "uploaded_files",
+      req.file.originalname
+    );
+    const parsedData = await parsePdf(pdfFilePath);
+    res.status(200).json({
+      message: "File uploaded and parsed successfully",
+      file: req.file,
+      parsedData,
+    });
+  } catch (error) {
+    console.error("Error parsing PDF:", error);
+    res.status(500).json({ error: "Error parsing PDF" });
+  }
+});
+
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
